e201f33bb54107f58782e0080b3963180070f4c7
Ziv Zamechek <ziv921@yahoo.com>
2016-12-30 15:44:10 -0500

    Changed database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161230195401) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
